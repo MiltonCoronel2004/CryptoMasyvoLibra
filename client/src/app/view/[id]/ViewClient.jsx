fb44770539d4e0494d771e72e4136e36eddaad58
Milton <miltoncoronel2004@gmail.com>
2025-09-28 03:02:51 -0300

    commit

--- client/src/app/view/[id]/ViewClient.jsx
+++ client/src/app/view/[id]/ViewClient.jsx
@@ -9,7 +9,9 @@ export default function ViewClient({ id }) {
 
     ws.onmessage = (event) => {
       const response = JSON.parse(event.data);
-      setCrypto()
+      console.log(response);
+
+      setCrypto(response.data);
     };
 
     return () => ws.close();
@@ -20,14 +22,11 @@ export default function ViewClient({ id }) {
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
         <legend className="fieldset-legend">Page details</legend>
 
-        <label className="label">Title</label>
-        <input type="text" className="input" placeholder="My awesome page" />
+        <input type="text" className="input" value={crypto?.s ?? "Crypto"} disabled />
 
-        <label className="label">Slug</label>
-        <input type="text" className="input" placeholder="my-awesome-page" />
+        <input type="text" className="input" value={crypto?.c ?? 0} disabled />
 
-        <label className="label">Author</label>
-        <input type="text" className="input" placeholder="Name" />
+        <input type="text" className="input" placeholder="Name" disabled />
       </fieldset>
     </main>
   );
