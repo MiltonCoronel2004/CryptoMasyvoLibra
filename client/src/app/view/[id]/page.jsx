72e7f841098fc1f29df3c83ec76e73d8f9c6cfc3
Milton <miltoncoronel2004@gmail.com>
2025-10-18 00:05:43 -0300

    added favicon

--- client/src/app/view/[id]/page.jsx
+++ client/src/app/view/[id]/page.jsx
@@ -1,5 +1,12 @@
 import ViewClient from "./ViewClient";
 
+export const metadata = {
+  title: "View Crypto",
+  icons: {
+    icon: "../bitcoin.png",
+  },
+};
+
 export default async function ViewPage({ params }) {
   const { id } = await params;
 
